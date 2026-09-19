664f7b1409e7bf514a40f334c061ddbbfd854883
yhprollyyy <nateiscoolinn@gmail.com>
2026-09-18 22:57:29 -0400

    Wire module settings into FreecamModule

--- myclient/src/main/java/com/example/myclient/modules/FreecamModule.java
+++ myclient/src/main/java/com/example/myclient/modules/FreecamModule.java
@@ -1,29 +1,24 @@
 package com.example.myclient.modules;
 
 import net.minecraft.client.Minecraft;
+import com.example.myclient.settings.DoubleSetting;
 
 public class FreecamModule extends Module {
     private boolean oldNoGravity;
+    private final DoubleSetting speed=addSetting(new DoubleSetting("Speed",1.0,0.1,5.0,0.1));
+    private final DoubleSetting verticalSpeed=addSetting(new DoubleSetting("VerticalSpeed",1.0,0.1,5.0,0.1));
 
-    public FreecamModule() { super("Freecam"); }
+    public FreecamModule(){super("Freecam");}
 
-    @Override
-    protected void onEnable() {
-        Minecraft mc = Minecraft.getInstance();
-        if (mc.player != null) {
-            oldNoGravity = mc.player.isNoGravity();
-            mc.player.setNoGravity(true);
-        }
+    @Override protected void onEnable(){
+        Minecraft mc=Minecraft.getInstance();
+        if(mc.player!=null){oldNoGravity=mc.player.isNoGravity();mc.player.setNoGravity(true);}
     }
-
-    @Override
-    protected void onDisable() {
-        Minecraft mc = Minecraft.getInstance();
-        if (mc.player != null) mc.player.setNoGravity(oldNoGravity);
+    @Override protected void onDisable(){
+        Minecraft mc=Minecraft.getInstance();
+        if(mc.player!=null)mc.player.setNoGravity(oldNoGravity);
     }
-
-    @Override
-    public void onTick(Minecraft mc) {
-        if (mc.player != null) mc.player.setNoGravity(true);
+    @Override public void onTick(Minecraft mc){
+        if(mc.player!=null)mc.player.setNoGravity(true);
     }
 }
