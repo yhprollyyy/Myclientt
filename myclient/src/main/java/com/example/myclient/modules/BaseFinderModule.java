9e9960d76beec6d291ba68fdc343d18d051b38b0
yhprollyyy <nateiscoolinn@gmail.com>
2026-09-18 22:57:24 -0400

    Wire module settings into BaseFinderModule

--- myclient/src/main/java/com/example/myclient/modules/BaseFinderModule.java
+++ myclient/src/main/java/com/example/myclient/modules/BaseFinderModule.java
@@ -4,45 +4,40 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.npc.Villager;
 import net.minecraft.world.level.block.Blocks;
-
+import com.example.myclient.settings.BooleanSetting;
+import com.example.myclient.settings.IntSetting;
 import java.util.HashSet;
 import java.util.Set;
 
 public class BaseFinderModule extends Module {
     private final Set<BlockPos> reported = new HashSet<>();
     private int tick;
+    private final IntSetting range = addSetting(new IntSetting("Range",32,8,64));
+    private final BooleanSetting villagers = addSetting(new BooleanSetting("Villagers",true));
+    private final BooleanSetting chests = addSetting(new BooleanSetting("Chests",true));
+    private final BooleanSetting spawners = addSetting(new BooleanSetting("Spawners",true));
 
-    public BaseFinderModule() { super("BaseFinder"); }
+    public BaseFinderModule(){super("BaseFinder");}
 
-    @Override
-    public void onTick(Minecraft mc) {
-        if (mc.level == null || mc.player == null || ++tick % 10 != 0) return;
-        int r = 32;
-        BlockPos origin = mc.player.blockPosition();
+    @Override public void onTick(Minecraft mc){
+        if(mc.level==null||mc.player==null||++tick%10!=0)return;
+        int r=range.get(), rr=r*r;
+        BlockPos origin=mc.player.blockPosition();
 
-        for (Villager villager : mc.level.getEntitiesOfClass(Villager.class,
-                mc.player.getBoundingBox().inflate(r))) {
-            BlockPos pos = villager.blockPosition();
-            if (reported.add(pos)) {
-                mc.player.displayClientMessage(
-                        net.minecraft.network.chat.Component.literal(
-                                "[BaseFinder] Villager at " + pos.getX() + " " + pos.getY() + " " + pos.getZ()), false);
+        if(villagers.get()){
+            for(Villager v:mc.level.getEntitiesOfClass(Villager.class,mc.player.getBoundingBox().inflate(r))){
+                if(v.distanceToSqr(mc.player)>rr)continue;
+                BlockPos pos=v.blockPosition();
+                if(reported.add(pos)) msg(mc,"Villager at "+pos.getX()+" "+pos.getY()+" "+pos.getZ());
             }
         }
-
-        for (BlockPos pos : BlockPos.betweenClosed(
-                origin.offset(-r, -r, -r), origin.offset(r, r, r))) {
-            var block = mc.level.getBlockState(pos).getBlock();
-            if ((block == Blocks.CHEST || block == Blocks.TRAPPED_CHEST || block == Blocks.SPAWNER)
-                    && reported.add(pos.immutable())) {
-                mc.player.displayClientMessage(
-                        net.minecraft.network.chat.Component.literal(
-                                "[BaseFinder] " + block.getName().getString() + " at "
-                                        + pos.getX() + " " + pos.getY() + " " + pos.getZ()), false);
-            }
+        for(BlockPos pos:BlockPos.betweenClosed(origin.offset(-r,-r,-r),origin.offset(r,r,r))){
+            var block=mc.level.getBlockState(pos).getBlock();
+            boolean match=(chests.get()&&(block==Blocks.CHEST||block==Blocks.TRAPPED_CHEST))||(spawners.get()&&block==Blocks.SPAWNER);
+            if(match&&reported.add(pos.immutable()))
+                msg(mc,block.getName().getString()+" at "+pos.getX()+" "+pos.getY()+" "+pos.getZ());
         }
     }
-
-    @Override
-    protected void onDisable() { reported.clear(); }
+    private void msg(Minecraft mc,String s){mc.player.displayClientMessage(net.minecraft.network.chat.Component.literal("[BaseFinder] "+s),false);}
+    @Override protected void onDisable(){reported.clear();tick=0;}
 }
