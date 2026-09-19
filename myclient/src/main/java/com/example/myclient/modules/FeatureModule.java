b494548f7bb41046b7e7523f0b24d138f21aaf0d
yhprollyyy <nateiscoolinn@gmail.com>
2026-09-18 23:45:52 -0400

    Clean module registry entries

--- myclient/src/main/java/com/example/myclient/modules/FeatureModule.java
+++ myclient/src/main/java/com/example/myclient/modules/FeatureModule.java
@@ -1,10 +1,7 @@
 package com.example.myclient.modules;
 
-import com.example.myclient.settings.BooleanSetting;
-
 public class FeatureModule extends Module {
     private final String category;
-    private final BooleanSetting implemented = addSetting(new BooleanSetting("Implemented", false));
 
     public FeatureModule(String name, String category) {
         super(name);
